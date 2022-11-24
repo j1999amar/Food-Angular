d29c10b232a49787b37f3c35a9301fcdc22f2e22
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-11-25 00:02:36 +0530

    Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,26 @@ import { PizzaListComponent } from './pizza-list/pizza-list.component';
 import { BurgerListComponent } from './burger-list/burger-list.component';
 import { DessertListComponent } from './dessert-list/dessert-list.component';
 import { BeveragetListComponent } from './beveraget-list/beveraget-list.component';
-
+import { RouterModule,Routes  } from '@angular/router';
+const myRouter:Routes=[
+  {
+    path:"",
+    component:MenuComponent
+  },
+  {
+    path:"pizza",
+    component:PizzaListComponent
+  },{
+    path:"burger",
+    component:BurgerListComponent
+  },{
+    path:"dessert",
+    component:DessertListComponent,
+  },{
+    path:"beverage",
+    component:BeveragetListComponent,
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +37,8 @@ import { BeveragetListComponent } from './beveraget-list/beveraget-list.componen
     BeveragetListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(myRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
