72a4df169c338b28a7feb7cd79aeec29b99498c1
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-11-24 23:15:48 +0530

    Pizza List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { PizzaListComponent } from './pizza-list/pizza-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    PizzaListComponent
   ],
   imports: [
     BrowserModule
